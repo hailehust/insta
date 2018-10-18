8ecab7d281bb9565c88646c90bafe20b7774aaab
le van hai <hailevan96.hedspi@gmail.com>
2018-10-18 15:49:30 +0700

    create edit profile

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+
+  devise_for :users,
+    path: '',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+  # get '/users/:id', to: 'users#show'
+  # /users/3 -> Users controller, show action, params {id: '3'}
+  resources :users, only: [:show]
+end
